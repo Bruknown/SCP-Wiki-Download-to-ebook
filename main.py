9443d37da4a41a606010981018bfa369c12bc59e
SandWood Jones <sandwoodjones@gmail.com>
2020-10-20 23:07:28 -0300

    Making progress, decided to clean webpages later and simplified correct link matching by realizing I don't need to leave the website and using regex

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import requests
+import re
 import bs4
 import yaml
+
+re.MULTILINE = True
 
 initialSite = requests.get("http://www.scpwiki.com/")
 siteBuffer = [initialSite]
@@ -12,37 +15,23 @@ with open("blacklist.yaml") as f: blacklisted = yaml.load(f, Loader=yaml.FullLoa
 
 def siteGet(targeted_websites):
 	target = targeted_websites.pop() # remove site from buffer
-	soup = bs4.BeautifulSoup(target.content, "html.parser")
-	cleanSoup(soup)
+	curSoup = bs4.BeautifulSoup(target.content, "html.parser")
 	addedSites.append(target.url) # add the url to list of already downloaded sites
-	for newLink in getLinks(target, soup):
+	for newLink in getLinks(target, curSoup):
 		if newLink not in addedSites:
 			siteBuffer.append(newLink)
-	open(soup.title.string + ".html", "w+").write(soup.prettify())
+	open(curSoup.title.string + ".html", "w+").write(curSoup.prettify())
 
-def cleanSoup(dirty_soup): # removes the bulk of unwanted links
-	dirtyTags = []
-	for script in dirty_soup.find_all("script"): dirtyTags.append(script) # add it for cleaning
-	for div in dirty_soup.find_all("div"): # for all divs in webpage
-		if div.has_attr("class"): # handle div's classes
-			for foo in div["class"]:
-				if foo in blacklisted["classes"]:
-					if div not in dirtyTags: dirtyTags.append(div)
-		if div.has_attr("id"):
-			if div["id"] in blacklisted["ids"]:
-				if div not in dirtyTags: dirtyTags.append(div)
-	for tag in dirtyTags:
-		tag.decompose()
-
-def getLinks(parent_link, cleaned_soup): # get all non-blacklisted links from a webpage
+def getLinks(parent_link, cleaned_soup):
 	newLinks = []
-	for link in cleaned_soup.find_all("a"):
-		if link.has_attr("href"):
-			if link["href"] not in blacklisted["links"]:
-				if str(link["href"])[0] == "/":
-					newLinks.append(parent_link.url + str(link["href"])[1:]) # relative link to absolute url
-				else:
-					newLinks.append(link["href"])
+	for link in cleaned_soup.find_all("a"): # all links
+		if link.has_attr("href"): 
+			if link.get("href")[0] == "/": # if a link does not direct to an external site (we want to stay inside the wiki)
+				if not any(re.search(exp, link.get("href")) for exp in blacklisted["links_re"]): # matches every link with multiple regex patterns
+					absoluteLink = parent_link.url + str(link["href"])[1:] # relative link to absolute url
+					newLinks.append(absoluteLink)
 	return newLinks
+
+#TODO: after downloading all the links go back to them and remove sidebars, licenses, media links etc
 
 siteGet(siteBuffer)
